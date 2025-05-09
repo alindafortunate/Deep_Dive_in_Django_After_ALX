5a81d750520ba499683551a9e9c1024cbc0519b0
alindafortunate <alindafortunate5@gmail.com>
2025-05-09 21:37:42 +0300

    Added a comment form to forms.py

--- blog_site/blog/models.py
+++ blog_site/blog/models.py
@@ -61,3 +61,6 @@ class Comment(models.Model):
     class Meta:
         ordering = ["created"]
         indexes = [models.Index(fields=["created"])]
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
