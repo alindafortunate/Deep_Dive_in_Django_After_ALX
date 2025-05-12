cfce78d194a1cd71ebdc9aaa5f51cda2a73cee66
alindafortunate <alindafortunate5@gmail.com>
2025-05-12 16:20:17 +0300

    Added a method two though commented for looping through and rendering form fields

--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -24,7 +24,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("blog/", include("blog.urls", namespace="blog")),
+    path("", include("blog.urls", namespace="blog")),
 ]
 
 
