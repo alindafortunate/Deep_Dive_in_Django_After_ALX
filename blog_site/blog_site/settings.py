a020a2279c5603371558a161808f4a5bbb6506f3
alindafortunate <alindafortunate5@gmail.com>
2025-05-13 12:19:02 +0300

    Made more edits

--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -117,10 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
