68150e92ae5f2b6ea831d94ba7c7f6d3ca742d22
alindafortunate <alindafortunate5@gmail.com>
2025-05-13 14:46:25 +0300

    Changed MEDIA_URL

--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -122,11 +122,11 @@ if not DEBUG:
 
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # user uploaded files
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
