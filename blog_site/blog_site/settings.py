03ffd9411d9df0fa83c5e45a539b749ecce9860e
alindafortunate <alindafortunate5@gmail.com>
2025-05-13 16:41:10 +0300

    Turned debug off

--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -118,12 +118,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+if not DEBUG:
 
-
-# if not DEBUG:
-
-#     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
