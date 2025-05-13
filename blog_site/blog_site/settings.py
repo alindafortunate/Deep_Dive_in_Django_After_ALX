113ed38ebde7621c3c69a547a6e8998a75dcc982
alindafortunate <alindafortunate5@gmail.com>
2025-05-13 15:55:20 +0300

    Added STATICFILES_DIRS

--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -117,12 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-if not DEBUG:
 
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# if not DEBUG:
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # user uploaded files
 MEDIA_URL = "/media/"
