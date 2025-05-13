4bcd5d10fd485ec02f98bed97db7e33be1ca9adb
alindafortunate <alindafortunate5@gmail.com>
2025-05-13 14:52:57 +0300

    Added STATIC_ROOT and STATIC_URL to main urls.py

--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -26,7 +26,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls", namespace="blog")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
