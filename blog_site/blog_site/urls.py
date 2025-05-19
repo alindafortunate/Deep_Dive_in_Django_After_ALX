acd966baaac1ac1b81d3271a9bf07575cc86cc05
alindafortunate <alindafortunate5@gmail.com>
2025-05-19 16:51:39 +0300

    Re-implemented the sitemap functionality and it worked

--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -18,13 +18,20 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
 from django.urls import path, include
 
-
+sitemaps = {"posts": PostSitemap}
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls", namespace="blog")),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
 
 
