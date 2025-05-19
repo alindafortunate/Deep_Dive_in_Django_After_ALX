a15d59477dce3207551161563577160955b3e5d1
alindafortunate <alindafortunate5@gmail.com>
2025-05-19 16:32:44 +0300

    Added the path() settings to main urls.py file

--- blog_site/blog_site/urls.py
+++ blog_site/blog_site/urls.py
@@ -18,13 +18,21 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
+from blog.sitemaps import PostSiteMap
 
+sitemaps = {"posts": PostSiteMap}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls", namespace="blog")),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
 
 
