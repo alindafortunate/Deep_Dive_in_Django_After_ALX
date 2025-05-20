c4ddbb06578ccb26b88f00c903157f17ad06f7e5
alindafortunate <alindafortunate5@gmail.com>
2025-05-20 22:56:35 +0300

    Imported the reverse_lazy for constructing a url

--- blog_site/blog/feed.py
+++ blog_site/blog/feed.py
@@ -1,3 +1,9 @@
 import markdown
 from django.template.defaultfilters import truncatewords
 from django.contrib.syndication.views import Feed
+from django.urls import reverse_lazy
+from .models import Post
+
+class LatestPostFeed(Feed):
+    title='My Blog'
+    link=
