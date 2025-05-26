9e77a508a3dabd90da0fdc9546c2fa2ddcd296a4
alindafortunate <alindafortunate5@gmail.com>
2025-05-26 22:37:51 +0300

    Added a search.html file

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -128,3 +128,19 @@ def post_comment(request, post_id):
 # On 21st/May/2025 I was engaged with Building Tomorrow work, so I didn't code.
 # On 23rd/May/2025 I was engaged with the donor visit (Building Tomorrow work) and I didn't code.
 # On 24th/May/2025 I was engaged with the thanks giving ceremony of Madam Rhonah, and I didn't code.
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if query in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body")
+            ).filter(search=query)
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
+    )
